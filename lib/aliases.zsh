1a65dce962c1c340bb18e907258f7342d05b206f
Igor Stuzhuk (KoHcoJlb) <fujitsuigor@gmail.com>
2019-11-13 20:13:35 +0200

    git fetch && reset --hard alias

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -3,3 +3,12 @@ alias cp="rsync -Phrvl"
 function sudo-zsh() {
   sudo $@ ZDOTDIR=$HOME SUDO_ZSH=1 zsh
 }
+
+function git() {
+  if [[ $1 == "fpull" ]]
+  then
+    /usr/bin/git fetch && /usr/bin/git reset --hard $(git rev-parse --abbrev-ref --symbolic-full-name @{u})
+  else
+    /usr/bin/git $@
+  fi
+}
