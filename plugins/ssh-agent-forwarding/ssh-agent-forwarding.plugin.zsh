e85c24bf47fa5be12fc8cd9d0810dee49b23441e
Igor Stuzhuk (KoHcoJlb) <fujitsuigor@gmail.com>
2020-09-21 14:20:04 +0300

    fix ssh agent forwarding plugin

--- plugins/ssh-agent-forwarding/ssh-agent-forwarding.plugin.zsh
+++ plugins/ssh-agent-forwarding/ssh-agent-forwarding.plugin.zsh
@@ -1,7 +1,7 @@
 auth_sock_path=${ZSH_SSH_AGENT_SOCK:-$(cd;pwd)/.ssh/auth_sock}
 mkdir -p $(dirname $auth_sock_path)
 
-if [[ $SSH_AUTH_SOCK != $auth_sock_path ]]
+if [[ -n $SSH_AUTH_SOCK && $SSH_AUTH_SOCK != $auth_sock_path ]]
 then
     ln -sf $SSH_AUTH_SOCK $auth_sock_path
 fi
