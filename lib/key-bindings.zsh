e7986241080bd156eb6495a7e4df29f72b909c5a
Ihor Stuzhuk <fujitsuigor@gmail.com>
2025-10-17 13:48:20 +0300

    Change bindings

--- lib/key-bindings.zsh
+++ lib/key-bindings.zsh
@@ -102,13 +102,13 @@ bindkey -M viins '^[[3;5~' kill-word
 bindkey -M vicmd '^[[3;5~' kill-word
 
 # [Ctrl-RightArrow] - move forward one word
-bindkey -M emacs '^[[1;5C' forward-word
-bindkey -M viins '^[[1;5C' forward-word
-bindkey -M vicmd '^[[1;5C' forward-word
+bindkey -M emacs '^[[1;5C' emacs-forward-word
+bindkey -M viins '^[[1;5C' emacs-forward-word
+bindkey -M vicmd '^[[1;5C' emacs-forward-word
 # [Ctrl-LeftArrow] - move backward one word
-bindkey -M emacs '^[[1;5D' backward-word
-bindkey -M viins '^[[1;5D' backward-word
-bindkey -M vicmd '^[[1;5D' backward-word
+bindkey -M emacs '^[[1;5D' emacs-backward-word
+bindkey -M viins '^[[1;5D' emacs-backward-word
+bindkey -M vicmd '^[[1;5D' emacs-backward-word
 
 
 bindkey '\ew' kill-region                             # [Esc-w] - Kill from the cursor to the mark
